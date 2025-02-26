4a76f49bfe63dc25961bce3d0ff448725a0640cf
GRANDINMaxime <grandinmaxime@outlook.fr>
2025-02-26 16:51:59 +0100

    Models+Enum

---
+++ src/main/java/com/example/models/Format.java
@@ -0,0 +1,5 @@
+package com.example.models;
+
+public enum Format {
+    POCHE, BROCHE,GRAND_FORMAT
+}
---
+++ src/main/java/com/example/models/ReservationStatus.java
@@ -0,0 +1,6 @@
+package com.example.models;
+
+public enum ReservationStatus {
+    ACTIVE, CANCELLED
+}
+
